570539f9cfbabfb4e65d05b36633e140921187c9
AdibSuid <adibcom.as@gmail.com>
2025-12-15 11:41:27 +0800

    initial commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import datetime
 import threading
 import time
 
-from gstreamer_streamer import GStreamerStreamer as Streamer
+from streamer import Streamer
 from discovery import ONVIFDiscovery, scan_network_ports
 from monitor import NetworkMonitor, TelegramNotifier
 import cloud_uploader as cloud_uploader_module
